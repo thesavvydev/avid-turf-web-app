f67ba3a9733cd8701891fe4dcb5c3a9a69be0131
tony-alignnutrition <tony@align-nutrition.com>
2024-08-30 14:24:33 -0600

    Remove existing and add next-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import Hero from "@/components/hero";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-
 export default async function Index() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        <SignUpUserSteps />
-      </main>
-    </>
-  );
+  return <>home</>;
 }
