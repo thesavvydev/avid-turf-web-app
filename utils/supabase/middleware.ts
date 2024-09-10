7d85ded110d2f915cae97ed4208d31b08793cd3d
tony-alignnutrition <tony@thesavvy.dev>
2024-09-09 19:06:58 -0600

    Add auth validation back

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -35,16 +35,16 @@ export const updateSession = async (request: NextRequest) => {
 
   // This will refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/server-side/nextjs
-  // const user = await supabase.auth.getUser();
+  const user = await supabase.auth.getUser();
 
   // protected routes
-  // if (request.nextUrl.pathname.startsWith("/manage") && user.error) {
-  //   return NextResponse.redirect(new URL("/sign-in", request.url));
-  // }
+  if (request.nextUrl.pathname.startsWith("/manage") && user.error) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
 
-  // if (request.nextUrl.pathname === "/" && !user.error) {
-  //   return NextResponse.redirect(new URL("/manage", request.url));
-  // }
+  if (request.nextUrl.pathname === "/" && !user.error) {
+    return NextResponse.redirect(new URL("/manage", request.url));
+  }
 
   return response;
 };
