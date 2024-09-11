e9b5c3a408d57a492b91b7b89aa8b819153eacfb
tony-alignnutrition <tony@thesavvy.dev>
2024-09-11 07:02:12 -0600

    Update auth redirects

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,12 +2,14 @@ import { Database } from "@/types/supabase";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = () => {
+export const createClient = ({ admin = false }: { admin?: boolean } = {}) => {
   const cookieStore = cookies();
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    admin
+      ? process.env.SUPABASE_SERVICE_ROLE!
+      : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
