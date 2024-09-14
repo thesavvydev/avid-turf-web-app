e269b07db65f42f6d9167a60f2d699400c5fea74
tony-alignnutrition <tony@thesavvy.dev>
2024-09-14 10:11:49 -0600

    Remove unused admin routes

--- app/admin/use-admin-nav-items.tsx
+++ app/admin/use-admin-nav-items.tsx
@@ -1,11 +1,5 @@
 "use client";
-import {
-  CalendarDaysIcon,
-  HomeIcon,
-  MapPinIcon,
-  UserIcon,
-  WorkflowIcon,
-} from "lucide-react";
+import { HomeIcon, MapPinIcon, UserIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 export default function useAdminNavItems() {
@@ -23,18 +17,6 @@ export default function useAdminNavItems() {
       href: "/admin/locations",
       isActive: pathname.startsWith("/admin/location"),
       icon: MapPinIcon,
-    },
-    {
-      name: "Jobs",
-      href: "/admin/jobs",
-      isActive: pathname.startsWith("/admin/job"),
-      icon: WorkflowIcon,
-    },
-    {
-      name: "Appointments",
-      href: "/admin/appointments",
-      isActive: pathname.startsWith("/admin/appointment"),
-      icon: CalendarDaysIcon,
     },
     {
       name: "Users",
