9fb14e58335def6999115007725bf90f1b360f0e
tony-alignnutrition <tony@thesavvy.dev>
2024-09-26 09:54:59 -0600

    Add default value to initials split

--- utils/get-initials.ts
+++ utils/get-initials.ts
@@ -1,5 +1,5 @@
 export default function getInitials(word: string) {
-  const [firstWord = "", secondWord = ""] = word.split(" ");
+  const [firstWord = "", secondWord = ""] = word.split(" ") ?? "";
   const [firstInitial] = firstWord.split("");
   const [secondInitial] = secondWord.split("");
 
