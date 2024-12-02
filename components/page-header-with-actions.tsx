41f8f53415879254be9f21615985ec56df14e634
tony-alignnutrition <tony@thesavvy.dev>
2024-12-02 05:31:21 -0700

    Remove sticky from page header

--- components/page-header-with-actions.tsx
+++ components/page-header-with-actions.tsx
@@ -1,10 +1,8 @@
 import { ReactElement, ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
 
 type TPageHeaderWithActions = {
   isAdmin?: boolean;
   title: string | ReactElement;
-  sticky?: boolean;
   subtitle?: string | ReactElement;
   renderActions?: () => ReactNode;
   renderBreadcrumbs?: () => ReactNode;
@@ -12,7 +10,6 @@ type TPageHeaderWithActions = {
 
 export default function PageHeaderWithActions({
   title,
-  sticky = false,
   subtitle,
   renderActions,
   renderBreadcrumbs,
@@ -22,13 +19,7 @@ export default function PageHeaderWithActions({
       {renderBreadcrumbs && (
         <div className="sm:-mb-4">{renderBreadcrumbs()}</div>
       )}
-      <header
-        className={twMerge(
-          sticky
-            ? "sticky top-0 z-0 border-b border-gray-200 bg-gray-50 p-2 text-gray-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300"
-            : "",
-        )}
-      >
+      <header>
         <div className="flex flex-col items-start justify-between gap-2 sm:flex-row sm:gap-0">
           <hgroup>
             <h1 className="text-3xl font-semibold">{title}</h1>
