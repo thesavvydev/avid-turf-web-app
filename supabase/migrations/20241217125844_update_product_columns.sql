92b0b699ea25e5be27319c60ea903042d5c8a351
tony-alignnutrition <tony@thesavvy.dev>
2024-12-17 08:29:05 -0700

    Update migration to remove null constraints

--- supabase/migrations/20241217125844_update_product_columns.sql
+++ supabase/migrations/20241217125844_update_product_columns.sql
@@ -2,14 +2,14 @@ alter table "public"."business_location_job_products" drop column "lead_price_ad
 
 alter table "public"."business_location_job_products" add column "lead_price" double precision default '0'::double precision;
 
-alter table "public"."business_location_job_products" add column "unit_price" double precision not null default '0'::double precision;
+alter table "public"."business_location_job_products" add column "unit_price" double precision default '0'::double precision;
 
 alter table "public"."business_products" drop column "measurement";
 
 alter table "public"."business_products" drop column "price_per_measurement";
 
-alter table "public"."business_products" add column "unit" text not null default 'sq ft'::text;
+alter table "public"."business_products" add column "unit" text default 'sq ft'::text;
 
-alter table "public"."business_products" add column "unit_price" double precision not null;
+alter table "public"."business_products" add column "unit_price" double precision;
 
 
