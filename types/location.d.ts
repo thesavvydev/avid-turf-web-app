fdb15f418cbdf340361bfe122bd41a5105bcf1c1
tony-alignnutrition <tony@thesavvy.dev>
2024-12-27 19:03:46 -0700

    Add ILocationCustomer interface

--- types/location.d.ts
+++ types/location.d.ts
@@ -4,3 +4,9 @@ export interface ILocationEmployee
   extends Tables<"business_location_profiles"> {
   profile: Tables<"profiles"> | null;
 }
+
+export interface ILocationCustomer
+  extends Tables<"business_location_customers"> {
+  creator?: Tables<"profiles">;
+  jobs?: Tables<"business_location_jobs">[];
+}
